ea002902c26eb4f5333d10d463d294c7203ad389
Vikas Jaiswal <vikasjaiswal794@gmail.com>
2024-04-24 12:10:58 +0530

    added the mongodb configuration info

--- src/main/java/com/journalApp/Jorunal/JorunalApplication.java
+++ src/main/java/com/journalApp/Jorunal/JorunalApplication.java
@@ -12,3 +12,7 @@ public class JorunalApplication {
 		SpringApplication.run(JorunalApplication.class, args);
 	}
 }
+// password = fFVjrOb1UGlARFp4 
+// username = nasay27353
+// mongodb+srv://nasay27353:fFVjrOb1UGlARFp4@cluster0.psrui9t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+// mongodb+srv://nasay27353:<password>@cluster0.psrui9t.mongodb.net/
